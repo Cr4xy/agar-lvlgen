f58e3c8fb4f8a3ae37483ca427513d7fa2c67de3
W1LKC0 <ismael.cormier@outlook.com>
2016-03-28 14:32:07 -0400

    Update config.js
    Traduction FRANCAISE des instructions de configuration / FRENCH TRADUCTION of instruction for configure.

--- config.js
+++ config.js
@@ -8,20 +8,23 @@ module.exports = {
 		}
 	],
 
-	name: "agar-lvlgen", // Name of the bots that will be playing.
-	reset: 0, // Minutes before server reset. (0 for none)
-	showtoken: false, // Show the token of the Facebook account on start.
-	liveConsole: true, // Show a live console instead of new lines.
+	name: "agar-lvlgen", // Name of the bots that will be playing. // Ce champ requiert le nom du bot qui se connectera.
+	reset: 0, // Minutes before server reset. (0 for none) // Le nombre de minutes avant que le serveur redemarre.
+	showtoken: false, // Show the token of the Facebook account on start. // Afficher ou nom le token du compte facebook au demarrage.
+	liveConsole: true, // Show a live console instead of new lines. // Voir la console en mode reel pour les nouvelles lignes.
 
 	// Advance
 	regions: ["BR-Brazil", "CN-China", "EU-London", "JP-Tokyo", "RU-Russia", "SG-Singapore", "TK-Turkey", "US-Atlanta"],
-	statusDelay: 1000, // Delay of milleseconds for console.log. (1000 recommended for live console)
+	statusDelay: 1000, // Delay of milleseconds for console.log. (1000 recommended for live console) // Delais en miliseconde pour les logs console.
 	
-	/* Possible values:
+	/* Possible values: // Options possible:
 	 *
-	 * default: Main AI - Poor performance, and low CPU Usage.
-	 * apos: Beta AI - Better performance, and higher CPU Usage.
+	 * default: Main AI - Poor performance, and low CPU Usage. // default: Robot Autonome - Mauvaise performance, et basse usage du CPU.
+	 * apos: Beta AI - Better performance, and higher CPU Usage. // apos: Robot Beta - Meilleure performance, et haute usage du CPU.
 	 * 
 	*/
 	ai: "apos"
 }
+
+        /* Traduction FRENCH/FRANCAIS par W1LKC0
+        */
